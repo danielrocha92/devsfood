89f8e1831e5883d6df867d09a7f61c243a260bce
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2022-05-20 15:48:54 -0300

    Adicionando Tooltip

--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 import { 
     Container,
     CategoryArea,
@@ -25,6 +26,7 @@ export default () => {
             if(cat.error == '') {
                 setCategories(cat.result);
             }
+            ReactTooltip.rebuild();
         };
         getCategories();
     }, []);
@@ -45,7 +47,7 @@ export default () => {
                         <CategoryItem
                         data={{
                             id:0, 
-                            title:'Todas as categorias',
+                            name:'Todas as categorias',
                             image: '/assets/food-and-restaurant.png'
                         }}
                         activeCategory={activeCategory}
