3053926ddded969e998bb25f3a44e9545b189b11
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2022-05-21 20:59:28 -0300

    Filtrar Produtos

--- src/api.jsx
+++ src/api.jsx
@@ -7,9 +7,21 @@ export default {
         const json = await res.json();
         return json;
     },
-    getProducts: async () => {
-        //GET /products ([serach, page, category])
-        const res = await fetch(BASE+'/products');
+    getProducts: async (category, page, search) => {
+        //GET /products ([search, page, category])
+        
+        let fields = {};
+        if(category !== 0) {
+            fields.category = category;
+        }
+        if(page > 0) {
+            fields.page = page;
+        }
+        if(search != '') {
+            fields.search = search;
+        }
+        let queryString = new URLSearchParams(fields).toString();
+        const res = await fetch(BASE+'/products?'+queryString);
         const json = await res.json();
         return json;
     }
